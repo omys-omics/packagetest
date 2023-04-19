ed5360d251fba6f03492445a667d8db716d9edb3
Ben Wiens <bwiens36@gmail.com>
2023-04-19 16:54:37 -0500

    v 0.0.1 documentation

--- R/f1.R
+++ R/f1.R
@@ -1,3 +1,12 @@
+#' This is the f1 function
+#'
+#' @param text (character) the text to paste with f1. Default = "default"
+#'
+#' @return character vector of length 1
+#' @export
+#'
+#' @examples
+#' f <- f1(text = "default")
 f1 <- function(text = "default") {
   return(paste(text, "F1"))
 }
